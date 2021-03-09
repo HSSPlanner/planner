438d443468e9b8f930fc0443eddd31d0a4363be8
edwardzhang02 <edward.zhang.eo@gmail.com>
2021-03-09 16:38:25 -0500

    finally worked

--- sidenav.js
+++ sidenav.js
@@ -1,10 +1,16 @@
 function openNav() {
+
+  if(document.getElementById("mySidenav").style.width == "0px"){
+    console.log(document.getElementById("mySidenav").style.width + "test")
+    console.log("true");
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+
   }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  else {
+    console.log(document.getElementById("mySidenav").style.width + "testststs");
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+
   }
+}
