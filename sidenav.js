20a77952e6ac6792b04285f5041ab64e0c80c8de
edwardzhang02 <edward.zhang.eo@gmail.com>
2021-03-10 14:27:11 -0500

    added class blocks

--- sidenav.js
+++ sidenav.js
@@ -1,16 +1,20 @@
 function openNav() {
 
-  if(document.getElementById("mySidenav").style.width == "0px"){
+  if(document.getElementById("mySidenav").style.width == "250px"){
     console.log(document.getElementById("mySidenav").style.width + "test")
     console.log("true");
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-
-  }
-  else {
-    console.log(document.getElementById("mySidenav").style.width + "testststs");
     document.getElementById("mySidenav").style.width = "0px";
     document.getElementById("main").style.marginLeft = "0px";
 
   }
+  else {
+    console.log(document.getElementById("mySidenav").style.width + "testststs");
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "150px";
+
+  }
+}
+
+function extendfunc(){
+  if(document.getElementById("block1"))
 }
